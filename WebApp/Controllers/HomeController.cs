92241460f986d82333528f07b7bbcc197ea4f686
Heber Marques <hebermarques@pop-os.localdomain>
2022-11-23 08:06:07 -0300

    Added .net project

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["APP_NAME"] = _configuration["AppName"];
+        ViewData["MACHINE_NAME"] = Environment.MachineName;
+        ViewData["HOST_NAME"] = Request.Host.Value;
+        ViewData["REQUEST_PATH"] = Request.Path;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
